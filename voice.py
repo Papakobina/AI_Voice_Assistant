79baa8d0081f058d5add56822a74295333f2bd82
PapaKobina <pkkwegyi@uwaterloo.ca>
2024-05-22 00:53:47 -0400

    just reserached and found the audio listerner I want to use

--- voice.py
+++ voice.py
@@ -1,3 +1,19 @@
 #https://stackoverflow.com/questions/76560926/can-i-use-the-python-speech-recognition-and-openais-whisper-libraries-together
 
 import speech_recognition as sr
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+    try:
+        print("You said: " + r.recognize_whisper(audio, language="english"))
+    except sr.UnknownValueError:
+        print("could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        
+
+if __name__ == "__main__":
+    listen()
